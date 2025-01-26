ebcde0d21e33552670c03b1b25a6dcecb60d4b21
Abhigyat Sahu <abhigyatsahu281@gmail.com>
2025-01-27 00:41:26 +0530

    Initial commit

---
+++ telemedicine-platform/src/test/java/com/project/telemedicine/TelemedicinePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.telemedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicinePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
